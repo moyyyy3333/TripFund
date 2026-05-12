e6c16e14516702a9c649b52fd483f7c043c54e97
My Mac <mymac@MacBook-Pro-di-my.local>
2026-05-11 19:51:35 -0500

    Fix all internal links with basePath prefix

---
+++ _next/static/0U0TrFo07ryLdFXV7Oe-2/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/TripFund//_next/:path+",
+        "destination": "/TripFund/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
